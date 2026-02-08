5a8c2ad0e4e16b1e1e45ef8b259dc369204bf5e3
BruceLeeSon <raketa3322@mail.ru>
2026-02-08 19:57:31 +0300

    add explodable blocks

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 import arcade
 import constants
+import random
+
+class ExplodableBlock(arcade.Sprite):
+    def __init__(self):
+        super().__init__("Blocks/ExplodableBlock.png", 1)
+
 
 
 class SolidBlock(arcade.Sprite):
@@ -13,15 +19,22 @@ class Game(arcade.Window):
         super().__init__(width, height, title)
         self.bg_tile = arcade.load_texture("Blocks/BackgroundTile.png")
         self.solid_blocks = arcade.SpriteList()
+        self.explodable_blocks = arcade.SpriteList()
 
     def setup(self):
         for y in range(constants.ROW_COUNT):
             for x in range(constants.COLUMN_COUNT):
                 if x % 2 == 1 and y % 2 == 1:
                     solid_block = SolidBlock()
-                    solid_block.center_x = x * constants.CELL_WIDTH
-                    solid_block.center_y = y * constants.CELL_HEIGHT
+                    solid_block.center_x = x * constants.CELL_WIDTH + constants.CELL_WIDTH / 2
+                    solid_block.center_y = y * constants.CELL_HEIGHT + constants.CELL_HEIGHT / 2
                     self.solid_blocks.append(solid_block)
+                elif random.randint(1, 2) == 1:
+                    if not (x == 0 and y <= 2) and not (y == 0 and x <= 2):
+                        explodable_block = ExplodableBlock()
+                        explodable_block.center_x = x * constants.CELL_WIDTH + constants.CELL_WIDTH / 2
+                        explodable_block.center_y = y * constants.CELL_HEIGHT + constants.CELL_HEIGHT / 2
+                        self.explodable_blocks.append(explodable_block)
 
     def draw_background(self):
         for y in range(constants.ROW_COUNT):
@@ -39,6 +52,7 @@ class Game(arcade.Window):
         self.clear((255, 255, 255))
         self.draw_background()
         self.solid_blocks.draw()
+        self.explodable_blocks.draw()
 
     def update(self, delta_time: float):
         pass
